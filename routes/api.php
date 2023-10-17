db3f2922a1a71d1f3c4f9c0c814aa6f6bd52e259
Arie Setiadi <arie@eannovate.com>
2023-10-17 20:07:14 +0800

    remove api related stuff

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,5 @@
 <?php
 
-use App\Http\Controllers\API\V1\Auth\LoginController;
-use App\Http\Controllers\API\V1\Auth\LogoutController;
-use App\Http\Controllers\API\V1\Customer\CustomerController;
-use App\Http\Controllers\API\V1\Payment\PaymentController;
-use App\Http\Controllers\API\V1\User\UserController;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes Version 1
@@ -17,31 +10,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-// API Locale
-Route::middleware(['locale.use.api', 'throttle:100,1'])->group(function () {
-    // API Guest
-    Route::middleware('guest:sanctum')->group(function () {
-        Route::post('/auth/login/process', [LoginController::class, 'process']);
-    });
-
-    // API Authenticated
-    Route::middleware('auth:sanctum')->group(function () {
-        // API Logout
-        Route::delete('/auth/logout/process', [LogoutController::class, 'process']);
-
-        // API Payment
-        Route::prefix('/payment')->group(function () {
-            // Entry Point
-            Route::post('/', [PaymentController::class, 'index']);
-            // Callback
-            Route::post('/callback/{gateway}', [PaymentController::class, 'callback']);
-        });
-
-        // API Users
-        Route::apiResource('users', UserController::class);
-
-        // API Customers
-        Route::apiResource('customers', CustomerController::class);
-    });
-});
